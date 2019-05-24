c61ce0ae7aef0f9c5334d19cfa3bbf22dd92fe4a
Montique Willis <montique.willis@gmail.com>
2019-05-24 14:45:03 -0400

    Update manual-renew-certs-0.4.X.sh

--- tools/manual-renew-certs-0.4.X.sh
+++ tools/manual-renew-certs-0.4.X.sh
@@ -5,7 +5,7 @@
 script_dir=$(readlink -f $(dirname "$0"))
 moziot_dir="/home/pi/mozilla-iot"
 moziot_profile_dir="/home/pi/.mozilla-iot"
-moziot_email="certificate@mozilla-iot.org"
+moziot_email="monti@growcomputer.com"
 pagekite_pidfile="/tmp/_pagekite.pid"
 temp_dir="$(mktemp -d)"
 server_pid=""
@@ -49,14 +49,14 @@ domain="$(sqlite3 "${moziot_profile_dir}/config/db.sqlite3" \
               "SELECT value FROM settings WHERE key='tunneltoken'" | \
           grep -oP '"name":".*?"' | \
           cut -d: -f2 | \
-          cut -d\" -f2).mozilla-iot.org"
+          cut -d\" -f2).growhub.io"
 token=$(sqlite3 "${moziot_profile_dir}/config/db.sqlite3" \
             "SELECT value FROM settings WHERE key='tunneltoken'" | \
         grep -oP '"token":".*?"' | \
         cut -d: -f2 | \
         cut -d\" -f2)
 
-if [ "${domain}" = ".mozilla-iot.org" -o -z "${token}" ]; then
+if [ "${domain}" = ".growhub.io" -o -z "${token}" ]; then
     abort "Could not determine domain or token."
 fi
 
@@ -140,7 +140,7 @@ cp "${moziot_profile_dir}/etc/live/${domain}/chain.pem" \
     "${moziot_profile_dir}/ssl/chain.pem"
 
 echo "Registering domain with server."
-curl "https://api.mozilla-iot.org:8443/setemail" \
+curl "https://api.growhub.io:8443/setemail" \
     -s -G \
     --data-urlencode "token=${token}" \
     --data-urlencode "email=${email}" || abort "Failed to register."
